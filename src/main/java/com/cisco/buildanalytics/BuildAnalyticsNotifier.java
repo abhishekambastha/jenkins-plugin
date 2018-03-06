15a816e9f1c851c2ff30c95be570129049c43802
Abhishek <aambasth@cisco.com>
2018-03-06 13:16:57 +0530

    Adding and refining the DTO for analytics server

--- src/main/java/com/cisco/buildanalytics/BuildAnalyticsNotifier.java
+++ src/main/java/com/cisco/buildanalytics/BuildAnalyticsNotifier.java
@@ -24,182 +24,216 @@
 
 package com.cisco.buildanalytics;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import com.google.gson.Gson;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.*;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+import hudson.tasks.Publisher;
+import jenkins.tasks.SimpleBuildStep;
+import jenkins.util.VirtualFile;
+
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.springframework.util.CollectionUtils;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
-
-import org.jenkinsci.Symbol;
-import org.kohsuke.stapler.DataBoundConstructor;
-
-import com.google.gson.Gson;
-
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Launcher;
-import hudson.model.AbstractBuild;
-import hudson.model.AbstractProject;
-import hudson.model.BuildListener;
-import hudson.model.Result;
-import hudson.model.Run;
-import hudson.model.TaskListener;
-import hudson.tasks.BuildStepDescriptor;
-import hudson.tasks.BuildStepMonitor;
-import hudson.tasks.Notifier;
-import hudson.tasks.Publisher;
-import jenkins.tasks.SimpleBuildStep;
 
 public class BuildAnalyticsNotifier extends Notifier implements SimpleBuildStep {
 
-	private static final Logger LOG = Logger.getLogger(BuildAnalyticsNotifier.class.getName());
-	private static String CHARSET = "UTF-8";
-	private static final Gson gson = new Gson();
+    private static final Logger LOG = Logger.getLogger(BuildAnalyticsNotifier.class.getName());
+    private static String CHARSET = "UTF-8";
+    private static final Gson gson = new Gson();
 
-	public String serverIp;
-	public String buildStageType;
-	public String filebeatsDirectory;
-	public String userPrefix;
-	public String jenkinsServerIp;
-	public boolean uploadOnlyOnFail;
-	public boolean failBuild;
+    public String serverIp;
+    public String buildStageType;
+    public String filebeatsDirectory;
+    public String userPrefix;
+    public String jenkinsServerIp;
+    public boolean uploadOnlyOnFail;
+    public boolean failBuild;
 
-	@DataBoundConstructor
-	public BuildAnalyticsNotifier(String serverIp, String buildStageType, String filebeatsDirectory, String userPrefix,
-			String jenkinsServerIp, boolean uploadOblyOnFail, boolean failBuild) {
-		this.serverIp = serverIp;
-		this.buildStageType = buildStageType;
-		this.filebeatsDirectory = filebeatsDirectory;
-		this.userPrefix = userPrefix;
-		this.jenkinsServerIp = jenkinsServerIp;
-		this.uploadOnlyOnFail = uploadOblyOnFail;
-		this.failBuild = failBuild;
-	}
+    @DataBoundConstructor
+    public BuildAnalyticsNotifier(String serverIp, String buildStageType, String filebeatsDirectory, String userPrefix,
+                                  String jenkinsServerIp, boolean uploadOblyOnFail, boolean failBuild) {
+        this.serverIp = serverIp;
+        this.buildStageType = buildStageType;
+        this.filebeatsDirectory = filebeatsDirectory;
+        this.userPrefix = userPrefix;
+        this.jenkinsServerIp = jenkinsServerIp;
+        this.uploadOnlyOnFail = uploadOblyOnFail;
+        this.failBuild = failBuild;
+    }
 
-	@Override
-	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
-		return perform(build, listener);
-	}
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+        return perform(build, listener);
+    }
 
-	@Override
-	public boolean needsToRunAfterFinalized() {
-		return true;
-	}
+    @Override
+    public boolean needsToRunAfterFinalized() {
+        return true;
+    }
 
-	@Override
-	public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
-			throws InterruptedException, IOException {
-		if (!perform(run, listener)) {
-			run.setResult(Result.FAILURE);
-		}
-	}
+    @Override
+    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
+            throws InterruptedException, IOException {
+        if (!perform(run, listener)) {
+            run.setResult(Result.FAILURE);
+        }
+    }
 
-	private void createSymbolicLink(Path src, Path target) {
-		LOG.info("Creating symbolic link from " + target + " to " + src);
-		try {
-			Files.createSymbolicLink(src, target);
-		} catch (IOException e) {
-			LOG.info(e.toString());
-		}
-	}
+    private void createSymbolicLink(Path src, Path target) {
+        LOG.info("Creating symbolic link from " + target + " to " + src);
+        try {
+            Files.createSymbolicLink(src, target);
+        } catch (IOException e) {
+            LOG.info(e.toString());
+        }
+    }
 
-	private void invokeAnalyticsAPI(String buildUrl, String filename) {
-		BuildParamsDTO dto = new BuildParamsDTO();
-		dto.setBuildStageType(this.buildStageType);
-		dto.setFileName(filename);
-		dto.setJenkinsServerIp(this.jenkinsServerIp);
-		dto.setBuildUrl(buildUrl);
-		String result = gson.toJson(dto);
+    private void invokeAnalyticsAPI(Run<?, ?> run, String log_index) {
+        BuildParamsDTO dto = new BuildParamsDTO();
 
-		LOG.info("DTO: " + result);
-		postRequest(result);
-	}
+        dto.setBuild_type(this.buildStageType);
+        dto.setBuild_log_index(log_index);
+        dto.setJenkins_base_url(this.jenkinsServerIp);
+        dto.setBuild_user_prefix(this.userPrefix);
+        dto.setBuild_type(this.buildStageType);
 
-	private void postRequest(String result) {
-		try {
-			URL url = new URL(this.serverIp);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
+        List<Cause> causes = run.getCauses();
+        StringBuilder causeStr = new StringBuilder();
+        if (!CollectionUtils.isEmpty(causes)) {
+            for (Cause c : causes) {
+                causeStr.append(c.toString());
+                causeStr.append(".");
+                LOG.info("Cause: " + c.toString());
+            }
+        }
+        dto.setBuild_cause(causeStr.toString());
 
-			OutputStream os = conn.getOutputStream();
-			os.write(result.getBytes());
-			os.flush();
+        dto.setBuild_name(run.getFullDisplayName());
+        dto.setBuild_number(run.getNumber());
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        dto.setBuild_url(run.getUrl());
 
-			String output;
-			LOG.info("Output from server");
-			while ((output = br.readLine()) != null) {
-				LOG.info(output);
-			}
+        //Artifacts
+        List<String> artifacts = new ArrayList<>();
+        try {
+            LOG.info("artifact root" + run.pickArtifactManager().root());
+            for (VirtualFile l : run.pickArtifactManager().root().list()) {
+                LOG.info("File Name" + l.getName());
+                if(l.isDirectory()){
+                    for(VirtualFile ff: l.list()){
+                        artifacts.add(ff.getName() + "+" + ff.toURI());
+                    }
+                }else {
+                    artifacts.add(l.getName() + "+" + l.toURI());
+                }
+            }
+        } catch (IOException e) {
+            LOG.info("error!!");
+        }
 
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			LOG.warning(e.toString());
-		} catch (IOException e) {
-			LOG.warning(e.toString());
-		}
+        dto.setArtifactList(artifacts);
 
-	}
+        String result = gson.toJson(dto);
 
-	private boolean perform(Run<?, ?> run, TaskListener listener) {
-		LOG.info("Build Analytics Plugin - Running");
-		LOG.info("Parameters " + this.serverIp + this.uploadOnlyOnFail + this.buildStageType + this.jenkinsServerIp
-				+ this.userPrefix);
+        LOG.info("DTO: " + result);
+        postRequest(result);
+    }
 
-		boolean success = false;
-		Result r = run.getResult();
-		if (r != null) {
-			success = r.isCompleteBuild();
-		} else {
-			LOG.info("Result is null" + r.toString());
-		}
+    private void postRequest(String result) {
+        try {
+            URL url = new URL(this.serverIp);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
 
-		if (!this.uploadOnlyOnFail || (this.uploadOnlyOnFail && !success)) {
-			File file = run.getLogFile();
-			String filename = this.userPrefix + "-x-" + this.buildStageType + "-x-" + run.getId();
-			Path newLink = Paths.get(this.filebeatsDirectory + "/" + filename + ".log");
-			Path target = Paths.get(file.getAbsolutePath());
-			createSymbolicLink(newLink, target);
+            OutputStream os = conn.getOutputStream();
+            os.write(result.getBytes());
+            os.flush();
 
-			String buildUrl = run.getUrl();
-			invokeAnalyticsAPI(buildUrl, filename);
-		} else {
-			LOG.info("Skipping upload as requested");
-		}
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-		return !(failBuild);
-	}
+            String output;
+            LOG.info("Output from server");
+            while ((output = br.readLine()) != null) {
+                LOG.info(output);
+            }
 
-	public BuildStepMonitor getRequiredMonitorService() {
-		return BuildStepMonitor.NONE;
-	}
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            LOG.warning(e.toString());
+        } catch (IOException e) {
+            LOG.warning(e.toString());
+        }
 
-	@Override
-	public Descriptor getDescriptor() {
-		return (Descriptor) super.getDescriptor();
-	}
+    }
 
-	@Extension
-	@Symbol("logstashSend")
-	public static class Descriptor extends BuildStepDescriptor<Publisher> {
+    private boolean perform(Run<?, ?> run, TaskListener listener) {
+        LOG.info("Build Analytics Plugin - Running");
+        LOG.info("Parameters " + this.serverIp + this.uploadOnlyOnFail + this.buildStageType + this.jenkinsServerIp
+                + this.userPrefix);
 
-		@Override
-		public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType) {
-			return true;
-		}
+        boolean success = false;
+        Result r = run.getResult();
+        if (r != null) {
+            success = r.isCompleteBuild();
+        } else {
+            LOG.info("Result is null" + r.toString());
+        }
 
-		public String getDisplayName() {
-			return "Upload Logs For Analysis";
-		}
-	}
+        if (!this.uploadOnlyOnFail || (this.uploadOnlyOnFail && !success)) {
+            File file = run.getLogFile();
+            String filename = this.userPrefix + "-x-" + this.buildStageType + "-x-" + run.getId();
+            Path newLink = Paths.get(this.filebeatsDirectory + "/" + filename + ".log");
+            Path target = Paths.get(file.getAbsolutePath());
+            createSymbolicLink(newLink, target);
+
+            String buildUrl = run.getUrl();
+
+            invokeAnalyticsAPI(run, filename);
+        } else {
+            LOG.info("Skipping upload as requested");
+        }
+
+        return !(failBuild);
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    @Override
+    public Descriptor getDescriptor() {
+        return (Descriptor) super.getDescriptor();
+    }
+
+    @Extension
+    @Symbol("logstashSend")
+    public static class Descriptor extends BuildStepDescriptor<Publisher> {
+
+        @Override
+        public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+
+        public String getDisplayName() {
+            return "Upload Logs For Analysis";
+        }
+    }
 }
--- src/main/java/com/cisco/buildanalytics/BuildParamsDTO.java
+++ src/main/java/com/cisco/buildanalytics/BuildParamsDTO.java
@@ -1,58 +1,88 @@
 package com.cisco.buildanalytics;
 
-public class BuildParamsDTO {
-	private String serverIp;
-	private String buildStageType;
-	private String jenkinsServerIp;
-	private String prefixUser;
-	private String buildUrl;
-	private String fileName;
-	
-	public String getServerIp(){
-		return serverIp;
-	}
-	
-	public void setServerIp(String serverIp){
-		this.serverIp = serverIp;
-	}
-	
-	public String getBuildStageType(){
-		return buildStageType;
-	}
-	
-	public void setBuildStageType(String buildStageType){
-		this.buildStageType = buildStageType;
-	}
-	
-	public String getJenkinsServerIp(){
-		return jenkinsServerIp;
-	}
-	
-	public void setJenkinsServerIp(String jenkinsServerIp){
-		this.jenkinsServerIp = jenkinsServerIp;
-	}
+import java.util.List;
 
-	public String getPrefixUser(){
-		return prefixUser;
-	}
-	
-	public void setPrefixUser(String prefixUser){
-		this.prefixUser = prefixUser;
-	}
-	
-	public String getBuildUrl(){
-		return buildUrl;
-	}
-	
-	public void setBuildUrl(String buildUrl){
-		this.buildUrl = buildUrl;
-	}
-	
-	public String getFileName(){
-		return fileName;
-	}
-	
-	public void setFileName(String fileName){
-		this.fileName = fileName;
-	}
+public class BuildParamsDTO {
+
+    private String jenkins_base_url;
+    private String build_type;
+    private String build_name;
+    private String build_cause;
+    private int build_number;
+    private String build_user_prefix;
+    private String build_log_index;
+    private String build_url;
+    private List<String> artifactList;
+
+    public String getJenkins_base_url() {
+        return jenkins_base_url;
+    }
+
+    public void setJenkins_base_url(String jenkins_base_url) {
+        this.jenkins_base_url = jenkins_base_url;
+    }
+
+    public String getBuild_type() {
+        return build_type;
+    }
+
+    public void setBuild_type(String build_type) {
+        this.build_type = build_type;
+    }
+
+    public String getBuild_name() {
+        return build_name;
+    }
+
+    public void setBuild_name(String build_name) {
+        this.build_name = build_name;
+    }
+
+    public String getBuild_cause() {
+        return build_cause;
+    }
+
+    public void setBuild_cause(String build_cause) {
+        this.build_cause = build_cause;
+    }
+
+    public int getBuild_number() {
+        return build_number;
+    }
+
+    public void setBuild_number(int build_number) {
+        this.build_number = build_number;
+    }
+
+    public String getBuild_user_prefix() {
+        return build_user_prefix;
+    }
+
+    public void setBuild_user_prefix(String build_user_prefix) {
+        this.build_user_prefix = build_user_prefix;
+    }
+
+    public String getBuild_log_index() {
+        return build_log_index;
+    }
+
+    public void setBuild_log_index(String build_log_index) {
+        this.build_log_index = build_log_index;
+    }
+
+    public List<String> getArtifactList() {
+        return artifactList;
+    }
+
+    public void setArtifactList(List<String> artifactList) {
+        this.artifactList = artifactList;
+    }
+
+    public String getBuild_url() {
+        return build_url;
+    }
+
+    public void setBuild_url(String build_url) {
+        this.build_url = build_url;
+    }
 }
